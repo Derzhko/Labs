6431b57faa5db7eb4e7d58f6f8187e63599871c3
Alex <a.derzhko@yandex.ru>
2021-04-06 23:33:48 +0300

    add something

--- L1/main.py
+++ L1/main.py
@@ -1,4 +1,4 @@
-from math import cos
+from math import cos, pi, fabs
 g_l = 1.62
 M = 2150
 v_p = 3660
@@ -7,15 +7,34 @@ m = 200
 H_0 = 570
 v_0 = 70
 
+H = H_0
+v = v_0
 
 def autopilot(angle, delta_m, time):
-    a = -1 * g_l + cos(angle) / M * delta_m
-    v = a * time + v_0
-    H = H_0 + v_0 * time + cos(angle) * a * pow(time, 2) / 2
+    if angle == 180:
+        angle = pi
+    a = fabs(-1 * g_l * cos(angle) + (-1) * cos(angle) / M * delta_m * v_p)
+    v = (-1) * a * time * cos(angle) + v_0
+    H = H_0 + v_0 * time + (-1) * cos(angle) * a * pow(time, 2) / 2
+
     return v, H
 
 
-H = H_0
+
 
 while H > 0:
-    delta_m = (a_max + g_l) * M
+    if v > 0:
+        delta_m = (a_max - g_l) * M / v_p
+        time = v / a_max
+        m -= delta_m * time
+        v, H = autopilot(0, delta_m, time)
+        v_0, H_0 = v, H
+        print(v, H)
+    else:
+        a = g_l
+        time = pow(2 * H / a, 0.5)
+        delta_m = (a + g_l) * M / v_p
+        if delta_m * time < m:
+            v, H = autopilot(180, delta_m, time)
+            v_0, H_0 = v, H
+            print(v, H)
